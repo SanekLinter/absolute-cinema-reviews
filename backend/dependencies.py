23770c16585016dc20c795c267026d14eba6209d
SanekLinter <sanekedaklassno@gmail.com>
2025-11-22 23:15:54 +0300

    add /api/auth/me route and get_current_user dep func

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,7 +1,12 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
-from jose import jwt
 from config import settings
+import models, database, config
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES=1440
 
@@ -11,3 +16,26 @@ def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(database.get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Not authenticated",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, config.settings.SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = int(payload["sub"])
+        username: str = payload["username"]
+        role: str = payload["role"]
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
