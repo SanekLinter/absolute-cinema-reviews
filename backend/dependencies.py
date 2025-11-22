cecc909c3056c99f6737a051e94fd23bf6d03d19
SanekLinter <sanekedaklassno@gmail.com>
2025-11-22 16:57:52 +0300

    add /api/auth/login route

---
+++ backend/dependencies.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from config import settings
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=1440
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
