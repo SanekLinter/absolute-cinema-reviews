7c335b81595dd65bdab874785ebc1766ca6c449f
SanekLinter <sanekedaklassno@gmail.com>
2025-11-26 01:37:07 +0300

    delete unused import OAuth2PasswordRequestForm

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 import models, schemas, database
 from utils import hash_password, verify_password
