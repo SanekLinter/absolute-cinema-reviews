1250e7f69d83125ce2c06f81842e484c3f1c7e82
SanekLinter <sanekedaklassno@gmail.com>
2025-12-07 03:58:19 +0300

    add 2 blank lines

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -9,12 +9,14 @@ import models, database, config
 bearer_scheme = HTTPBearer()
 ALGORITHM = "HS256"
 
+
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)):
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
 
 def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
