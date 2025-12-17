dfc0a8065f80679f3bbce1ece91bfa1cb0bb1b3a
polina <epi-funny@mail.ru>
2025-12-18 01:12:10 +0300

    feat: add MyReviewsPage

--- frontend/src/lib/routes.ts
+++ frontend/src/lib/routes.ts
@@ -9,4 +9,7 @@ export const getMyReviewsRoute = () => '/my-reviews';
 export const getModerationRoute = () => '/moderation';
 
 export const getReviewRoute = (reviewId: number) => `/reviews/${reviewId}`;
+
 export const getUserProfileRoute = (userId: number) => `/users/${userId}`;
+
+export const getNewReviewRoute = () => '/reviews/new';
